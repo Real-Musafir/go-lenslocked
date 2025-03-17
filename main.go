a728e6d21cb818476cddd0e431988412ece5cc4f
alshahadath <abdullah.alshahadath@vstinc.com>
2025-03-18 00:40:39 +0600

    bring error in running time

--- main.go
+++ main.go
@@ -12,5 +12,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		panic(err)
+	}
 }
