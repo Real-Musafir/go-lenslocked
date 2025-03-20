cf0dbb2f8ba1354dad0e0037e80b30235b379a44
alshahadath <abdullah.alshahadath@vstinc.com>
2025-03-20 17:36:03 +0600

    chnaged the route type with structure

--- main.go
+++ main.go
@@ -15,7 +15,20 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:alshahadath@gmail.com\">alshahadath@gmail.com</a></p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found ", http.StatusNotFound)
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -27,10 +40,9 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
-	// http.HandleFunc("/contact", contactHandler)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	err := http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", router)
 
 	if err != nil {
 		panic(err)
