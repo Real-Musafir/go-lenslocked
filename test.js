5b166acfc0971e95a5d13a6499b8c73893f6987c
real-musafir <alshahadath@gmail.com>
2025-07-03 19:20:42 +0600

    Using Join in the SessionService

--- test.js
+++ test.js
@@ -1,44 +1,27 @@
-const input_array = [1, -1, 2];
-let left_sum = input_array[0];
-let right_sum = input_array[input_array.length - 1];
+import { memo, useState } from "react";
 
-let leftPosition = 0;
-let rightPosition = input_array.length - 1;
-let findedMiden = null;
-while (true) {
-  console.log(leftPosition, rightPosition, "Check the position");
-  if (leftPosition >= rightPosition) break;
-  if (
-    right_sum === 0 &&
-    leftPosition === 0 &&
-    rightPosition - leftPosition == 1
-  ) {
-    findedMiden = 0;
-    break;
-  }
-  if (
-    left_sum === 0 &&
-    rightPosition === input_array.length - 1 &&
-    rightPosition - leftPosition == 1
-  ) {
-    findedMiden = rightPosition;
-    break;
-  }
-
-  if (left_sum > right_sum) {
-    rightPosition = rightPosition - 1;
-    right_sum = right_sum + input_array[rightPosition];
-  } else if (left_sum < right_sum) {
-    leftPosition = leftPosition + 1;
-    left_sum = left_sum + input_array[leftPosition];
-  } else if (left_sum === right_sum && rightPosition - leftPosition === 2) {
-    findedMiden = leftPosition + 1;
-    break;
-  } else {
-    leftPosition = leftPosition + 1;
-    rightPosition = rightPosition - 1;
-    right_sum = right_sum + input_array[rightPosition];
-    left_sum = left_sum + input_array[leftPosition];
-  }
+const EmployeeProfile = memo(function EmployeeProfile({ name, email }) {
+  return (
+    <>
+      <p>Name:{name}</p>
+      <p>Email: {email}</p>
+    </>
+  );
+});
+export default function EmployeeRegForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  return (
+    <>
+      <label>
+        Name: <input value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Email:{" "}
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <hr />
+      <EmployeeProfile name={name} />
+    </>
+  );
 }
-console.log(findedMiden, "OKKK");
